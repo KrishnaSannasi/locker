3471affc95c791baf52c82fc7e261f1bd28ba165
Ozaren <krishna.sd.2012@gmail.com>
2020-03-02 17:41:24 -0700

    minor fix

--- locker/src/condvar/raw.rs
+++ locker/src/condvar/raw.rs
@@ -4,7 +4,7 @@ use parking_lot_core::{
 
 use super::{Parkable, WaitTimeoutResult};
 use crate::exclusive_lock::{raw::RawExclusiveGuard, RawExclusiveLock};
-use crate::share_lock::{raw::RawShareGuard, RawShareLock};
+use crate::share_lock::{RawShareGuard, RawShareLock};
 use crate::RawLockInfo;
 
 use std::ptr;
